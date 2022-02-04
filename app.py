ec9b6e6ea658d3c4a97590b99638c0123e0d3447
Houcem CHAABANE <houcem.chaabane@aqsone.com>
2022-02-04 15:33:14 +0100

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 from streamlit import write
 from transformers import pipeline
+from google.cloud import firestore
 import os
 
 #def app():
@@ -15,11 +16,18 @@ import os
 
 if __name__ == '__main__':
 
-
+    db = firestore.Client.from_service_account_info(st.secrets["gcp_service_account"])
     unmasker = pipeline('fill-mask', model='distilbert-base-uncased')
     input = st.text_input('Put your text')
     if len(input):
         st.write("Your input is :\n" + input)
-        st.write(pd.DataFrame(unmasker(input)))
+        result = unmasker(input)
+        st.write(pd.DataFrame(result))
+
+    if st.button("Store result in the database"):
+        data = {
+            u"table_results": result
+        }
+        db.collection("posts").document(input).set(data)
     #app()
-    # test 
+    # test
