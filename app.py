045822258a63032e38e1c654c71cd0ef6689406e
hchaabane <90621646+hchaabane@users.noreply.github.com>
2022-02-04 14:57:07 +0100

    send script to git

---
+++ app.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import streamlit as st
+from streamlit import write
+from transformers import pipeline
+import os
+
+#def app():
+#
+#    st.write("This is my first app:") #Affichage d'un titre dans l'app
+
+
+
+
+
+
+if __name__ == '__main__':
+
+
+    unmasker = pipeline('fill-mask', model='distilbert-base-uncased')
+    input = st.text_input('Put your text')
+    if len(input):
+        st.write("Your input is :\n" + input)
+        st.write(pd.DataFrame(unmasker(input)))
+    #app()
